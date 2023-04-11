ed2e02ca27a42715725f583ab8bcad359a3e3beb
Wiktor <wktr.chojnacki@gmail.com>
2023-04-11 16:34:29 +0200

    Color scheme

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,14 +5,16 @@ import styles from "./button.module.scss";
 interface ButtonProps {
   caption: string;
   primary?: boolean;
+  clickHandler?: () => void;
 }
 
-export const Button = ({ caption, primary }: ButtonProps) => {
+export const Button = ({ caption, primary, clickHandler }: ButtonProps) => {
   return (
     <button
       className={`${primary ? styles.primary : styles.secondary} ${
         styles.button
       }`}
+      onClick={clickHandler}
     >
       {caption}
     </button>
